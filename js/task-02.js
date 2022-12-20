7a352bcf03ad6b0a89dbf6a44cac4a61905699c4
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2022-12-20 20:40:09 +0200

    решение задачи №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector(`#ingredients`);
+
+ingredients.forEach(function (title) {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = title;
+  itemEl.classList = `item`;
+  
+  ingredientsEl.append(itemEl)
+})
