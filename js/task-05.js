30ce8fe29481065314de95b7b06c5743cb622962
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2022-12-22 14:10:55 +0200

    правки к задаче №5

--- js/task-05.js
+++ js/task-05.js
@@ -3,11 +3,16 @@ const spanRef = document.querySelector(`#name-output`)
 
 inputRef.addEventListener(`input`, (event) => {
 
-    if (event.currentTarget.value === ``) {
-        return spanRef.textContent = `Anonymous`;
-    }
-
-    if (event)
-    {return spanRef.textContent = event.currentTarget.value}
-    
+event.currentTarget.value
+    ? (spanRef.textContent = event.currentTarget.value)
+    : (spanRef.textContent = "Anonimus");    
 })
+
+
+// другий варіант 
+ // if (event.currentTarget.value === ``) {
+    //     return spanRef.textContent = `Anonymous`;
+    // }
+
+    // if (event)
+    // {return spanRef.textContent = event.currentTarget.value}
