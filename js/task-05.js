3d0da37aadab6c050a85461b33974005b699e95e
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2022-12-22 13:57:19 +0200

    решение задачи №5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector(`#name-input`)
+const spanRef = document.querySelector(`#name-output`)
+
+inputRef.addEventListener(`input`, (event) => {
+
+    if (event.currentTarget.value === ``) {
+        return spanRef.textContent = `Anonymous`;
+    }
+
+    if (event)
+    {return spanRef.textContent = event.currentTarget.value}
+    
+})
